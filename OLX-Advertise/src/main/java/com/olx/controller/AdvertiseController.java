c4bf714a761d54fc37a09d333748bebfbd0f8f3b
prashantdhoke23 <99012605+prashantdhoke23@users.noreply.github.com>
2022-04-06 09:18:26 +0530

    Add files via upload

---
+++ OLX-Advertise/src/main/java/com/olx/controller/AdvertiseController.java
@@ -0,0 +1,12 @@
+package com.olx.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/olx/advertise")
+@CrossOrigin(origins = "*")
+public class AdvertiseController {
+
+}
